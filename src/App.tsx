fdbd29af992f486d26347a0831a5fe51b094f405
Mina Hosseini <minaa.hosseinii@gmail.com>
2021-05-18 22:23:47 +0200

    question card set up

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,17 @@
 import React, { MouseEvent, useState } from 'react';
 import './App.css';
+import QuestionCard from './components/QuestoinCard';
 
 
 
 
 const App:React.FC = () => {
+  const [loading, setLoading] = useState(false);
+  const [questions, setQuestions] = useState([]);
+  const [number, setNumber] = useState(0);
+  const [userAnswers, setUserAnswers] = useState([]);
+  const [score, setScore] = useState(0);
+  const [gameover, setGameover] = useState(true );
 
   const startTrivia = async() => {
 
@@ -21,6 +28,19 @@ const App:React.FC = () => {
 
   return (
     <div className="App">
+      <h1>React quiz with Typescript</h1>
+      <button className="start" onClick={startTrivia}>Start</button>
+      <p className="score">Score:</p>
+      <p>Loading... </p>
+      <QuestionCard 
+        questionNr={number + 1}
+        totalQuestions={10}
+        question={questions[number].question}
+        answers={questions[number].answer}
+        userAnswer={userAnswers ? userAnswers[number] : undefined}
+        callback={checkAnswer}
+      />
+      <button className="next" onClick={nextQuestion}>Next question</button>
     </div>
   );
 }
--- src/components/QuestoinCard.tsx
+++ src/components/QuestoinCard.tsx
@@ -1,8 +1,36 @@
 import React from 'react';
 
-const QuestionCard = () => {
+
+interface Props {
+  question: string;
+  answers: string[];
+  callback: any;
+  userAnswer: any;
+  questionNr: number;
+  totalQuestions: number; 
+}
+const QuestionCard: React.FC<Props> = ({question, 
+  answers, 
+  callback, 
+  userAnswer, 
+  questionNr, 
+  totalQuestions}) => {
   return (
-    <div>Question Card</div>
+    <div>
+      <p className="number">
+        Question:{questionNr} / {totalQuestions}
+      </p>
+      <p dangerouslySetInnerHTML={{__html: question}}></p>
+      <div>{
+        answers.map(answer => 
+          <div>
+            <button disabled={userAnswer} onClick={callback}>
+              <span dangerouslySetInnerHTML={{__html: answer}}></span>
+            </button>
+          </div>
+        )}
+      </div>
+    </div>
   )
 }
 
