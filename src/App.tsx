79ea58311fef93d85504802119894ccfbf20e5d9
Mina Hosseini <minaa.hosseinii@gmail.com>
2021-05-18 10:38:56 +0200

    initial setup

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,26 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { MouseEvent, useState } from 'react';
 import './App.css';
 
-function App() {
+
+
+
+const App:React.FC = () => {
+
+  const startTrivia = async() => {
+
+  }
+  const checkAnswer = (event: React.MouseEvent<HTMLButtonElement>) => {
+
+  }
+
+  const nextQuestion = () => {
+
+  }
+  
+  
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
     </div>
   );
 }
